a9d4601028cec2e64a330c65f65d4c6c8088273b
mikey <michal_maslanka@onet.pl>
2024-05-23 18:05:11 +0200

    skibidi

--- bank/MainWindow.xaml.cs
+++ bank/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +17,63 @@ namespace bank;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private float balance;
+    private float wplat;
+    private float wyplat;
     public MainWindow()
     {
         InitializeComponent();
+
+    }
+
+    private void ButtonWplac_OnClick(object sender, RoutedEventArgs e)
+    {
+        string value = TextBox.Text;
+        TextBox.Text = "";
+
+        float fvalue = float.Parse(value);
+        
+        balance += fvalue;
+        LabelBalance.Content = balance.ToString("C", new CultureInfo("pl-PL"));
+        
+        wplat += fvalue;
+        LabelWplaty.Content = wplat.ToString("C", new CultureInfo("pl-PL"));
+
+        LabelMsg.Content = "dokonano wplaty: " + fvalue.ToString("C", new CultureInfo("pl-PL"));
+    }
+
+    private void ButtonWyplac_OnClick(object sender, RoutedEventArgs e)
+    {
+        string value = TextBox.Text;
+        TextBox.Text = "";
+
+        float fvalue = float.Parse(value);
+
+        if (fvalue > balance)
+        {
+            LabelMsg.Content = "nie udalo sie zrealizowac tej operacji";
+            return;
+        }
+
+        balance -= fvalue;
+        LabelBalance.Content = balance.ToString("C", new CultureInfo("pl-PL"));
+
+        wyplat += fvalue;
+        LabelWyplaty.Content = wyplat.ToString("C", new CultureInfo("pl-PL"));
+        
+        LabelMsg.Content = "dokonano wyplaty: " + fvalue.ToString("C", new CultureInfo("pl-PL"));
+    }
+
+    private void ButtonWyplacAll_OnClick(object sender, RoutedEventArgs e)
+    {
+        float fvalue = balance;
+
+        balance = 0;
+        LabelBalance.Content = balance.ToString("C", new CultureInfo("pl-PL"));
+        
+        wyplat += fvalue;
+        LabelWyplaty.Content = wyplat.ToString("C", new CultureInfo("pl-PL"));
+
+        LabelMsg.Content = "dokonano wyplaty: " + fvalue.ToString("C", new CultureInfo("pl-PL"));
     }
 }
